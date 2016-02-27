17a7321fd6165d1fa2d8d9f47b52bb7c93a0697c
Zack Bostian <bostianz@gmail.com>
2016-02-27 15:29:04 -0500

    added files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   output: {
@@ -7,17 +10,27 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+      loaders: [
+      {
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './',
+    hot: true
   }
 };
