11c7b3c86ee221c839ccf5cc8a580c270b6ba8db
Zack Bostian <bostianz@gmail.com>
2016-03-03 13:14:37 -0500

    added the about banner

--- src/Containers/About/AboutBanner.js
+++ src/Containers/About/AboutBanner.js
@@ -7,7 +7,7 @@ class AboutBanner extends Component {
     return (
       <div>
         <div className="aboutBanner"></div>
-        <h2 className="aboutBannerTagline">Some text Some text Some text Some text Some text </h2>
+        <h2 className="aboutBannerTagline">A place to share your story, and discover others.</h2>
       </div>
     );
   }
