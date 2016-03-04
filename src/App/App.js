8dce4a310147adcb6d3108236c88096f7441dd51
Zack Bostian <bostianz@gmail.com>
2016-03-04 10:50:56 -0500

    finished about section for now

--- src/App/App.js
+++ src/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        {this.props.children || <HomeContainer />}
+        {this.props.children}
       </div>
     );
   }
--- src/Containers/About/AboutContainer.js
+++ src/Containers/About/AboutContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import AboutBanner from './AboutBanner';
-import AboutSection from './AboutSection';
 
 class AboutContainer extends Component {
   render() {
 
     return (
       <div className="aboutContainer">
-        <AboutBanner />
+        <div className="aboutBanner">
+            <h2 className="aboutBannerTagline">A place to share your thoughts, and discover others.</h2>
+        </div>
+        <div className="aboutSectionOne">
+          <h2>Throw your thoughts onto paper, and watch them grow.</h2>
+          <p>Scribble is a place that allows users express an idea,
+            grow that idea, and share it with others.</p>
+          <h3>So. What are you thinking?</h3>
+        </div>
       </div>
     );
   }
