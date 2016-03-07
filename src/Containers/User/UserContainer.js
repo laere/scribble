dd930b3a85e559ee90e4256446d96b2d538c557c
Zack Bostian <bostianz@gmail.com>
2016-03-07 09:02:26 -0500

    added user profile content for now

--- src/Containers/User/UserContainer.js
+++ src/Containers/User/UserContainer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-
+import Section from '../../Components/Section';
 
 class UserContainer extends Component {
   render() {
     return (
-      <div>User</div>
+      <Section className="userContainer" header="Users' Name">
+        <p>Profile information That can be edited</p>
+        <span>Following 0</span>
+        <span>Followers 0</span>
+        <i className="fa fa-twitter fa-2x"></i>
+      </Section>
     );
   }
 }
